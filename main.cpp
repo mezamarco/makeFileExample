1930b3127b2d4e3ee23be66a30e196aa155e2e7f
Marco Meza <mezamarco6@gmail.com>
2018-04-04 23:02:24 -0700

    second commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "message.h"
+
+
+int main(){
+  
+  //Create out message object, then tell it to do a function.
+	
+
+  Message theMessageObject;
+
+    
+  std::cout << "The message was: " << theMessageObject.communicateFunct << std::endl;
+	
+
+}
+
